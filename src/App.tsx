63079417a20db9501d0f2cd5a05828cee719441b
Shekhar007284 <satyashekhar064@gmail.com>
2026-09-10 13:22:54 +0530

    a new commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import {
   AreaChart, Area, BarChart, Bar, XAxis, YAxis, CartesianGrid,
   Tooltip, ResponsiveContainer, Cell, RadialBarChart, RadialBar,
 } from "recharts";
-import { Analytics } from "@vercel/analytics/next";
 import {
   getPipeline, getMonthlyChart, getBranchStats, getAgingLeads,
   getLostReasons, getSourceStats, getKPIs, fmtCrore, fmtLakh,
